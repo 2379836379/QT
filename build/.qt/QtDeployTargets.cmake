65e7086fce0c8c538fc154d134b95cdaafaf0136
Tian <231277751+2379836379@users.noreply.github.com>
2026-05-20 10:12:51 +0800

    repair gitignore

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_oj_FILE E:/csqt/build/oj.exe)
-set(__QT_DEPLOY_TARGET_oj_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_oj_RUNTIME_DLLS D:/QT/6.11.0/mingw_64/bin/Qt6Widgets.dll;D:/QT/6.11.0/mingw_64/bin/Qt6Network.dll;D:/QT/6.11.0/mingw_64/bin/Qt6Sql.dll;D:/QT/6.11.0/mingw_64/bin/Qt6Gui.dll;D:/QT/6.11.0/mingw_64/bin/Qt6Core.dll)
