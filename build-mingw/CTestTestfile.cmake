46c3f201e9654e4bece7323b8743312c984ec03b
Tian <231277751+2379836379@users.noreply.github.com>
2026-07-01 05:34:11 +0800

    7.1

--- build-mingw/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/csqt/oj-client
-# Build directory: E:/csqt/build-mingw
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[oj_parser_tests]=] "E:/csqt/build-mingw/oj_parser_tests.exe")
-set_tests_properties([=[oj_parser_tests]=] PROPERTIES  _BACKTRACE_TRIPLES "E:/csqt/oj-client/CMakeLists.txt;227;add_test;E:/csqt/oj-client/CMakeLists.txt;0;")
